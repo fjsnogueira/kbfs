6e019434d3eee50205b0f7b1d03850566dbd828a
Frederick Akalin <akalin@keyba.se>
2016-11-15 18:18:37 -0800

    Fix dokan

--- test/run_dokan_test.go
+++ test/run_dokan_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/keybase/client/go/libkb"
 	"github.com/keybase/client/go/logger"
 	"github.com/keybase/kbfs/dokan"
 	"github.com/keybase/kbfs/libdokan"
@@ -29,15 +30,23 @@ func createEngine() Engine {
 	return e
 }
 
-func createUserDokan(t testing.TB, ith int, config *libkbfs.ConfigLocal,
-	opTimeout time.Duration) *fsUser {
+func createUserDokan(t testing.TB, ith int, username libkb.NormalizedUsername,
+	config *libkbfs.ConfigLocal, opTimeout time.Duration) *fsUser {
 	driveLetter := 'T' + byte(ith)
 	if driveLetter > 'Z' {
 		t.Error("Too many users - out of drive letters")
 	}
+	// TODO: Unlock on panic.
 	getDriveLetterLock(driveLetter).Lock()
+
 	ctx := context.Background()
 	ctx, cancelFn := context.WithCancel(ctx)
+	logTags := logger.CtxLogTags{
+		CtxUserKey: CtxOpUser,
+	}
+	ctx = logger.NewContextWithLogTags(ctx, logTags)
+	ctx = context.WithValue(ctx, CtxUserKey, username)
+
 	fs, err := libdokan.NewFS(ctx, config, logger.NewTestLogger(t))
 	if err != nil {
 		t.Fatal(err)
@@ -57,9 +66,10 @@ func createUserDokan(t testing.TB, ith int, config *libkbfs.ConfigLocal,
 	}
 
 	return &fsUser{
-		mntDir: mnt.Dir,
-		config: config,
-		cancel: cancelFn,
+		mntDir:   mnt.Dir,
+		username: username,
+		config:   config,
+		cancel:   cancelFn,
 		close: func() {
 			dokan.Unmount(mnt.Dir)
 			getDriveLetterLock(driveLetter).Unlock()
